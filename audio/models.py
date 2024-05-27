33a3c07c8f70b808dc768050ff969a780e89380b
Armin Mozafary <armin.arm90@gmail.com>
2024-05-28 00:05:55 +0330

    audio model

--- audio/models.py
+++ audio/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class AudioFile(models.Model):
+    title = models.CharField(max_length=100)
+    audio = models.FileField(upload_to='audio/')
+
+    def __str__(self):
+        return self.title
