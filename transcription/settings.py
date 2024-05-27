08fadc8a0d99441cb886be1d62e60d0bca558679
Armin Mozafary <armin.arm90@gmail.com>
2024-05-27 23:30:46 +0330

    database settings

--- transcription/settings.py
+++ transcription/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'transcription.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'transcription ',
+        'USER': 'postgres',
+        'PASSWORD': '13823330',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
