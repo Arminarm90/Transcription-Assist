69805b6699da90b052011e4f6726a90af6e00323
Armin Mozafary <armin.arm90@gmail.com>
2024-06-15 11:51:02 +0330

    adding models for units

--- audio/models.py
+++ audio/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 
+class Unit(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Lesson(models.Model):
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
+
 class AudioFile(models.Model):
     title = models.CharField(max_length=100)
     audio = models.FileField(upload_to='tracks/')
