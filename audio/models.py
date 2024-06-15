dd3cb0f6b5f21695d90a4b2aa18d80d07be219dd
Armin Mozafary <armin.arm90@gmail.com>
2024-06-15 20:32:49 +0330

    creating models for units

--- audio/models.py
+++ audio/models.py
@@ -12,7 +12,7 @@ class Lesson(models.Model):
     content = models.TextField()
 
     def __str__(self):
-        return self.title
+        return f"{self.unit} : {self.title}"
 
 class AudioFile(models.Model):
     title = models.CharField(max_length=100)
